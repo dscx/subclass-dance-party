a4484bb536c2fee15970805a5aeebf607b7c2707
dscx <dnchmbrln@gmail.com>
2014-07-28 21:50:35 -0700

    added 2nd dancer page and made it work. new class in css and styling.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,5 +1,7 @@
 var BlinkyDancer = function(top, left, timeBetweenSteps) {
   Dancer.apply(this, arguments);
+
+  console.log("this is blink "+Object.prototype.hasOwnProperty(this.setPosition));
 };
 
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
---
+++ src/colorDancer.js
@@ -0,0 +1,15 @@
+var ColorDancer = function(top, left, timeBetweenSteps) {
+  Dancer.apply(this, arguments);
+  this.$node = $('<span class="dancer_color"></span>');
+  this.setPosition(top, left);
+ console.log(Object.prototype.hasOwnProperty(this.setPosition)); 
+};
+
+ColorDancer.prototype = Object.create(Dancer.prototype);
+ColorDancer.prototype.constructor = ColorDancer;
+ColorDancer.prototype._oldStep = Dancer.prototype.step;
+ColorDancer.prototype.step = function() {
+  this._oldStep();
+  this.$node.fadeIn();
+};
+
